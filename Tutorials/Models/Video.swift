c5e3f8b4f40035ec5ff3a371c6c2e6a35ccc854c
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 17:41:26 -0400

    Added Video model file

---
+++ Tutorials/Models/Video.swift
@@ -0,0 +1,14 @@
+//
+//  Video.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/13/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+	let title: String
+	let url: String
+}
