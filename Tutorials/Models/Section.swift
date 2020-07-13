c4f0812cf5ba47cac1935ca5382ef0d4a97b88ae
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 17:39:05 -0400

    Added Section model file

---
+++ Tutorials/Models/Section.swift
@@ -0,0 +1,13 @@
+//
+//  Section.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/13/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+struct Section {
+	let title: String
+}
