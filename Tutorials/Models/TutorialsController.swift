8e3a1a52c58b524b0b040e795c1f249df4e021f0
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 17:44:06 -0400

    Added TutorialsController file

---
+++ Tutorials/Models/TutorialsController.swift
@@ -0,0 +1,16 @@
+//
+//  TutorialsController.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/13/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+// this type will be responsible for
+// bringing all our model types together
+
+struct TutorialsController {
+	
+}
