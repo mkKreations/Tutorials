a396283c4c069c52da5420a817ddfe5d674bfd11
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 18:12:02 -0400

    Successfully decoding plist within TutorialsController init

--- Tutorials/Models/TutorialsController.swift
+++ Tutorials/Models/TutorialsController.swift
@@ -11,6 +11,19 @@ import Foundation
 // this type will be responsible for
 // bringing all our model types together
 
-struct TutorialsController {
+// this type is a class so we can instantiate
+// our topics with decoded plist information
+
+class TutorialsController {
+	var topics: [Topic]
 	
+	init() {
+		guard let path = Bundle.main.path(forResource: "Tutorials", ofType: "plist"),
+					 let xml = FileManager.default.contents(atPath: path),
+	  let topicsData = try? PropertyListDecoder().decode([Topic].self, from: xml) else {
+			self.topics = [] // default if for whatever reason we can't reach our plist
+			return
+		}
+		topics = topicsData // we decoded our plist so set to internal property
+	}
 }
