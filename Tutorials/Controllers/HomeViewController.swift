6543a4ceef32e841f0c3496e899afb881363077f
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-12 20:02:56 -0400

    Added HomeViewController file

---
+++ Tutorials/Controllers/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/12/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .green
+	}
+}
