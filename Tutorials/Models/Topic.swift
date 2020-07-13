5b3e73ca512bae04a19ca6d3df76eac020f53cd8
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 17:34:30 -0400

    Added Topic model file

---
+++ Tutorials/Models/Topic.swift
@@ -0,0 +1,13 @@
+//
+//  Topic.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/13/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+struct Topic {
+	let title: String
+}
