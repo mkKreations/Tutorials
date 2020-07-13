f3b01477544128f4b5a50345075408ab7dc47a38
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 17:36:53 -0400

    Added Tutorial model file

---
+++ Tutorials/Models/Tutorial.swift
@@ -0,0 +1,18 @@
+//
+//  Tutorial.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/13/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import Foundation
+
+struct Tutorial {
+	let title: String
+	let thumbnail: String
+	let artworkColor: String
+	let publishDate: Date
+	let isQueued: Bool
+	let updateCount: Int
+}
