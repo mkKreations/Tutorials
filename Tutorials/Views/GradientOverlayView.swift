d74927a68b0f8ccaf045cea3972e19b8152403da
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-14 02:00:06 -0400

    Refactored GradientView so it appears and that it's not added unnecessarily

--- Tutorials/Views/GradientOverlayView.swift
+++ Tutorials/Views/GradientOverlayView.swift
@@ -9,9 +9,8 @@
 import UIKit
 
 class GradientOverlayView: UIView {
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-
+	// MARK: properties
+	private let gradientLayer: CAGradientLayer = {
 		// colors
 		let startColor = UIColor(white: 1.0, alpha: 0.0)
 		let midColor = UIColor(white: 0.0, alpha: 0.4)
@@ -26,11 +25,29 @@ class GradientOverlayView: UIView {
 		let gradient = CAGradientLayer()
 		gradient.colors = [startColor.cgColor, midColor.cgColor, endColor.cgColor]
 		gradient.locations = [startLocation, midLocation, endLocation]
-		gradient.frame = frame
-		layer.addSublayer(gradient)
-		layer.cornerRadius = 10.0
+		return gradient
+	}()
+	
+	
+	// MARK: inits
+	override init(frame: CGRect) {
+		super.init(frame: frame)
 	}
 	required init?(coder: NSCoder) {
 		fatalError("Crash in GradientOverlayView")
+	}
+	
+	
+	// MARK: layer layout
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		// only add layer if we have no sublayers
+		// we do not want to add it over and over
+		if layer.sublayers == nil {
+			gradientLayer.frame = bounds // set frame on our layer
+			layer.addSublayer(gradientLayer) // add layer as subLayer
+			layer.cornerRadius = 10.0 // set corner radius
+		}
 	}
 }
