374612509ef3de225bc185429a4ef9461c69c165
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 21:44:49 -0400

    Added TutorialCell file

---
+++ Tutorials/Views/TutorialCell.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialCell.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/13/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class TutorialCell: UICollectionViewCell {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in TutorialCell")
+	}
+}
