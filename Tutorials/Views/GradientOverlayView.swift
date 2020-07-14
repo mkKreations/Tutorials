53006342feda2f19a3289de58b4cbd91e3da3b62
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 22:21:01 -0400

    Added GradientOverlayView file

---
+++ Tutorials/Views/GradientOverlayView.swift
@@ -0,0 +1,18 @@
+//
+//  GradientOverlayView.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/13/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class GradientOverlayView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in GradientOverlayView")
+	}
+}
