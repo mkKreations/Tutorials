3cfc3c56e49ebb3c47ba881f234f4fd1fa15e89c
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-14 16:10:31 -0400

    Added implementation to TitleHeaderView - SEE MORE
    - Added titleLabel
    - Added reuseIdentifier

--- Tutorials/Views/TitleHeaderView.swift
+++ Tutorials/Views/TitleHeaderView.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class TitleHeaderView: UICollectionReusableView {
+	// reuseIdentifier
+	static let reuseIdentifier: String = String(describing: self)
+	
+	// private view properties
+	private let titleLabel = UILabel(frame: .zero)
+	
+	// public setters
+	var displayText: String? {
+		didSet {
+			titleLabel.text = displayText
+		}
+	}
+	
+	// inits
 	override init(frame: CGRect) {
 		super.init(frame: frame)
+		
+		configureTitleLabel()
 	}
 	required init?(coder: NSCoder) {
 		fatalError("Crash in TitleHeaderView")
+	}
+	
+	// private config methods
+	private func configureTitleLabel() {
+		titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		titleLabel.font = .preferredFont(forTextStyle: .title2)
+		addSubview(titleLabel)
+		
+		titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10.0).isActive = true
+		titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10.0).isActive = true
+		titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
 	}
 }
