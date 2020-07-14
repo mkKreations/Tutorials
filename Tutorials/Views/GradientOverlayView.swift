f98a37c8f75107d9b3116156846bbb6382eacdc7
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-13 22:26:26 -0400

    Added logic to create gradient within GradientOverlayView

--- Tutorials/Views/GradientOverlayView.swift
+++ Tutorials/Views/GradientOverlayView.swift
@@ -11,6 +11,24 @@ import UIKit
 class GradientOverlayView: UIView {
 	override init(frame: CGRect) {
 		super.init(frame: frame)
+
+		// colors
+		let startColor = UIColor(white: 1.0, alpha: 0.0)
+		let midColor = UIColor(white: 0.0, alpha: 0.4)
+		let endColor = UIColor(white: 0.0, alpha: 0.8)
+		
+		// locations
+		let startLocation: NSNumber = 0.0
+		let midLocation: NSNumber = 0.6
+		let endLocation: NSNumber = 1.0
+
+		// layer
+		let gradient = CAGradientLayer()
+		gradient.colors = [startColor.cgColor, midColor.cgColor, endColor.cgColor]
+		gradient.locations = [startLocation, midLocation, endLocation]
+		gradient.frame = frame
+		layer.addSublayer(gradient)
+		layer.cornerRadius = 10.0
 	}
 	required init?(coder: NSCoder) {
 		fatalError("Crash in GradientOverlayView")
