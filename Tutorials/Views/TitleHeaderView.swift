3a9b8b1526c0029fb0fda4c10eb513f2efa6f854
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-14 16:05:22 -0400

    Added TitleHeaderView file

---
+++ Tutorials/Views/TitleHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  TitleHeaderView.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/14/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class TitleHeaderView: UICollectionReusableView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in TitleHeaderView")
+	}
+}
