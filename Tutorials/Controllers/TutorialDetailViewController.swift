3eec6cb1bb17bcf0af0b33efc89121dbf0e7049a
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-15 00:25:02 -0400

    Added tutorialImageView to TutorialDetailViewController

--- Tutorials/Controllers/TutorialDetailViewController.swift
+++ Tutorials/Controllers/TutorialDetailViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class TutorialDetailViewController: UIViewController {
-	var tutorial: Tutorial! // we know we'll have a Tutorial
+	// MARK: dependencies
+	var tutorial: Tutorial! {
+		didSet {
+			tutorialImageView.image = UIImage(named: tutorial.thumbnail)
+		}
+	}
 	
+	
+	// MARK: subview properties
+	private let tutorialImageView = UIImageView(frame: .zero)
+	
+	
+	// MARK: view life cycle methods
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .systemOrange
+		
+		view.backgroundColor = .black
+		
+		edgesForExtendedLayout = []
+		
+		configureTutorialSubviews()
+		layoutTutorialSubviews()
+	}
+	
+	
+	// MARK: view configuration
+	private func configureTutorialSubviews() {
+		tutorialImageView.translatesAutoresizingMaskIntoConstraints = false
+		tutorialImageView.contentMode = .scaleAspectFit
+		view.addSubview(tutorialImageView)
+	}
+	private func layoutTutorialSubviews() {
+		tutorialImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		tutorialImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		tutorialImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+		tutorialImageView.heightAnchor.constraint(equalToConstant: 200.0).isActive = true
 	}
 }
