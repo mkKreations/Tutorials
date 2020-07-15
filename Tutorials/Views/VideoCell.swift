0cde08b08df32ceca4bf3f1d8ac7e76057516624
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-15 15:31:52 -0400

    Added VideoCell file

---
+++ Tutorials/Views/VideoCell.swift
@@ -0,0 +1,18 @@
+//
+//  VideoCell.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/15/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UICollectionViewCell {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in VideoCell")
+	}
+}
