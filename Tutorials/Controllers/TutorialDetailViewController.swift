1cf9d87eb3bd88692c154c8afb8b27404bc5d1a5
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-14 23:40:27 -0400

    Added TutorialDetailViewController file

---
+++ Tutorials/Controllers/TutorialDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TutorialDetailViewController.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/14/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class TutorialDetailViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .systemOrange
+	}
+}
