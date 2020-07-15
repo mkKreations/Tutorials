590750dd0ddc9cef48aa2c76b749a58a36ebcc30
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-15 12:41:40 -0400

    Added TutorialDetailTopView file

---
+++ Tutorials/Views/TutorialDetailTopView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialDetailTopView.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/15/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class TutorialDetailTopView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in TutorialDetailTopView")
+	}
+}
