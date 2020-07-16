bcf7adcb42d48de1e0e0188ccf24f3b2e9b9ef90
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-15 22:13:18 -0400

    Added QueuedViewController file

---
+++ Tutorials/Controllers/QueuedViewController.swift
@@ -0,0 +1,15 @@
+//
+//  QueuedViewController.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/15/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class QueuedViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
