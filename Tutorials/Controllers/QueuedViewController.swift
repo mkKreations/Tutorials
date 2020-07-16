b2dcc7bceadeab9190ee367a9fc9ceabf21115bd
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-15 22:15:18 -0400

    Added tabBarItem config to QueuedViewController

--- Tutorials/Controllers/QueuedViewController.swift
+++ Tutorials/Controllers/QueuedViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class QueuedViewController: UIViewController {
+	private lazy var queuedTabBarItem: UITabBarItem = {
+		UITabBarItem(title: title,
+								 image: UIImage(systemName: "bookmark"),
+								 selectedImage: UIImage(systemName: "bookmark.fill"))
+	}()
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		view.backgroundColor = UIColor.red
+		
+		tabBarItem = queuedTabBarItem
 	}
 }
