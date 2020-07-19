312ce6ee43d483d134354628c8bfb7991ead9614
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-19 11:55:17 -0400

    Added QueuedBadgeView file

---
+++ Tutorials/Views/QueuedBadgeView.swift
@@ -0,0 +1,18 @@
+//
+//  QueuedBadgeView.swift
+//  Tutorials
+//
+//  Created by Marcus on 7/19/20.
+//  Copyright © 2020 Marcus. All rights reserved.
+//
+
+import UIKit
+
+class QueuedBadgeView: UICollectionReusableView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in QueuedBadgeView")
+	}
+}
