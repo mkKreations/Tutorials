d1208d83c40dc7daf4c0549fda09cb733eb22277
mkKreations <63332239+mkKreations@users.noreply.github.com>
2020-07-19 12:06:48 -0400

    Added UI implementation of QueuedBadgeView

--- Tutorials/Views/QueuedBadgeView.swift
+++ Tutorials/Views/QueuedBadgeView.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class QueuedBadgeView: UICollectionReusableView {
+	// reuseIdentifier
+	static let reuseIdentifier: String = String(describing: self)
+	
+	// stored properties
+	private let bookmarkImageView = UIImageView(frame: .zero)
+	
+	// setters
+	var isQueued: Bool? {
+		didSet {
+			if let queued = isQueued {
+				// only show when Tutorial isQueued
+				bookmarkImageView.isHidden = !queued
+			}
+		}
+	}
+	
+	// inits
 	override init(frame: CGRect) {
 		super.init(frame: frame)
+
+		configureSubviews()
 	}
 	required init?(coder: NSCoder) {
 		fatalError("Crash in QueuedBadgeView")
+	}
+	
+	private func configureSubviews() {
+		bookmarkImageView.translatesAutoresizingMaskIntoConstraints = false
+		bookmarkImageView.image = UIImage(systemName: "bookmark.fill")
+		bookmarkImageView.isHidden = true // only show when Tutorial isQueued
+		addSubview(bookmarkImageView)
+		
+		bookmarkImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+		bookmarkImageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+		bookmarkImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+		bookmarkImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
 	}
 }
